f031f82ff46c41c97fadc29e5e3b289b02a0a460
cosmoplayit-pixel <cosmoplay.it@gmail.com>
2026-02-27 17:14:26 -0400

    Seeder

--- database/seeders/MultiEmpresaSeeder.php
+++ database/seeders/MultiEmpresaSeeder.php
@@ -8,13 +8,20 @@ class MultiEmpresaSeeder extends Seeder
 {
     public function run(): void
     {
+        // 1. Seeders obligatorios para producción y local
         $this->call([
             EmpresaSeeder::class,
             RolesAndUsersSeeder::class,
-            //EntidadesSeeder::class,
-            //ProyectosSeeder::class,
-            //BancosSeeder::class,
-            //AgenteServicioSeeder::class,
         ]);
+
+        // 2. Seeders de prueba solo para el entorno local
+        if (app()->environment('local')) {
+            $this->call([
+                EntidadesSeeder::class,
+                ProyectosSeeder::class,
+                BancosSeeder::class,
+                AgenteServicioSeeder::class,
+            ]);
+        }
     }
 }

be0986347fda3f6437bcd4df09589b609bac64ec
cosmoplayit-pixel <cosmoplay.it@gmail.com>
2026-03-13 15:10:52 -0400

    base de datos

--- database/migrations/2026_03_11_161250_create_dashboard_config_table.php
+++ database/migrations/2026_03_11_161250_create_dashboard_config_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
             $table->unsignedBigInteger('empresa_id')->unique()->nullable();
             $table->decimal('impuestos_nacionales_bob', 15, 2)->default(0);
             $table->decimal('impuestos_nacionales_usd', 15, 2)->default(0);
+            $table->decimal('tipo_cambio', 15, 2)->default(1.00);
+            $table->decimal('patrimonio_herramientas_bob', 15, 2)->default(0);
+            $table->decimal('patrimonio_herramientas_usd', 15, 2)->default(0);
+            $table->decimal('patrimonio_materiales_bob', 15, 2)->default(0);
+            $table->decimal('patrimonio_materiales_usd', 15, 2)->default(0);
+            $table->decimal('patrimonio_mobiliario_bob', 15, 2)->default(0);
+            $table->decimal('patrimonio_mobiliario_usd', 15, 2)->default(0);
+            $table->decimal('patrimonio_vehiculos_bob', 15, 2)->default(0);
+            $table->decimal('patrimonio_vehiculos_usd', 15, 2)->default(0);
+            $table->decimal('patrimonio_inmuebles_bob', 15, 2)->default(0);
+            $table->decimal('patrimonio_inmuebles_usd', 15, 2)->default(0);
             $table->timestamps();
 
             $table->foreign('empresa_id')->references('id')->on('empresas')->onDelete('cascade');
